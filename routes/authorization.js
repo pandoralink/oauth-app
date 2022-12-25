c860b0be8df7be2630a71f2504cf89cae388ea78
poplink <2219535001@qq.com>
2022-12-25 23:49:32 +0800

    feat: 新增第三方登录

---
+++ routes/authorization.js
@@ -0,0 +1,28 @@
+const { default: axios } = require("axios");
+const express = require("express");
+const router = express.Router();
+
+router.post("/redirect", function (req, res, next) {
+  const { code } = req.body;
+  axios({
+    method: "POST",
+    url: "https://github.com/login/oauth/access_token",
+    headers: {
+      Accept: "application/json",
+    },
+    timeout: 60 * 1000,
+    data: {
+      client_id: process.env.CLIENT_ID,
+      client_secret: process.env.CLIENT_SECRET,
+      code,
+    },
+  })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((e) => {
+      res.status(404);
+    });
+});
+
+module.exports = router;
